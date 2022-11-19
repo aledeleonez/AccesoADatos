cbcabae6e70922d0bb9531e28594ae368ebca4bc
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-19 11:39:40 +0100

    Proyecto creado

---
+++ Practica3.1/AppHibernate/src/main/java/com/example/apphibernate/AppHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.apphibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppHibernateApplication.class, args);
+    }
+
+}
