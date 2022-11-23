da0005bb56b25f4922cd65dfdd93a9f376853aa0
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-23 11:01:30 +0100

    metodos agregar y eliminar usuario

--- Practica3.1/src/Main.java
+++ Practica3.1/src/Main.java
@@ -3,16 +3,29 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String[] args){
 
-        HibernateUtil hibernateUtil = new HibernateUtil();
-
         Usuario usuario1 = new Usuario(null, "Alejandro", "1234", "aledeleonez", "ale@gmail.com");
+        agregarUsuario(usuario1);
+        //eliminarUsuario(usuario1);
 
+    }
+
+    private static void agregarUsuario(Usuario usr){
+        HibernateUtil hibernateUtil = new HibernateUtil();
         hibernateUtil.buildSessionFactory();
         Session session = HibernateUtil.getCurrentSession();
         session.beginTransaction();
-        session.save(usuario1);
+        session.save(usr);
         session.getTransaction().commit();
         session.close();
+    }
 
+    private static void eliminarUsuario(Usuario usr){
+        HibernateUtil hibernateUtil = new HibernateUtil();
+        hibernateUtil.buildSessionFactory();
+        Session session = HibernateUtil.getCurrentSession();
+        session.beginTransaction();
+        session.delete(usr);
+        session.getTransaction().commit();
+        session.close();
     }
 }
